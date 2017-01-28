57ee88ef186b744f2d497117be7f49d4bf6d1a4e
Kravalg <onlykrava@gmail.com>
2017-01-28 20:02:45 +0300

    improved start and end country buttons

--- index.android.js
+++ index.android.js
@@ -9,23 +9,27 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  Button,
   View
 } from 'react-native';
+
 
 export default class DriversRest extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between',}}>
+          <View style={{ width: 100, height: 100 }} >
+              <Button title="ENTER START COUNTRY" color="green" />
+          </View>
+          <View style={{ width: 100, height: 100 }} >
+              <Button title="ENTER END COUNTRY" color="red" />
+          </View>
+        </View>
+        <View style={{ borderColor: 'white', borderWidth: 1, width: 300}} >
+            <MovingBar
+                color={'green'}styleAttr="Horizontal" indeterminate={false} />
+        </View>
       </View>
     );
   }
@@ -34,20 +38,47 @@ export default class DriversRest extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#000000',
+    padding: 15
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+    color: '#fff',
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
+    color: '#fff',
     marginBottom: 5,
   },
+});
+
+var ProgressBar = require('ProgressBarAndroid');
+var TimerMixin = require('react-timer-mixin');
+
+var MovingBar = React.createClass({
+ mixins: [TimerMixin],
+ minutes: 10,
+ getInitialState: function() {
+     return {
+        progress: 0
+     };
+ },
+ getStep: function () {
+    return 1 / (this.minutes * 60);
+ },
+ componentDidMount: function() {
+     this.setInterval( () => {
+         var progress = (this.state.progress + this.getStep()) % 1;
+         this.setState({progress: progress});
+     }, 1000 );
+ },
+ render: function() {
+     return <ProgressBar
+             progress={this.state.progress} {...this.props}
+     />;
+ }
 });
 
 AppRegistry.registerComponent('DriversRest', () => DriversRest);
